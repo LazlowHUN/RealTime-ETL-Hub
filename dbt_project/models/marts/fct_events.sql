7a50e7bb9b5868745667fb5afe203d088ee9ffa9
LazlowHUN <doczilaci1111@gmail.com>
2025-11-17 19:28:11 +0100

    Add logic to fct_events

---
+++ dbt_project/models/marts/fct_events.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='table',
+    tags=['marts']
+) }}
+
+with base as (
+    select
+        user_id,
+        event_type,
+        date(ts) as event_date,
+        price,
+        quantity
+    from {{ ref('stg_events_flat') }}
+    where is_valid
+),
+
+agg as (
+    select
+        user_id,
+        event_type,
+        event_date,
+        count(*)                         as event_count,
+        sum(quantity)                    as total_quantity,
+        sum(price * quantity)            as total_revenue
+    from base
+    group by user_id, event_type, event_date
+)
+
+select *
+from agg
